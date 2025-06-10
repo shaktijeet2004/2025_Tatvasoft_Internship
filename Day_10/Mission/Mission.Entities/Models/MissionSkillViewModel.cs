fd73a3ee474f50acd22f9051535e21d7c72f29b2
Chauhan Shaktijeet Vijaysinh <148742345+shaktijeet2004@users.noreply.github.com>
2025-06-10 10:35:30 +0530

    Delete Day_10 directory

--- Day_10/Mission/Mission.Entities/Models/MissionSkillViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Mission.Entities.Models
-{
-    public class MissionSkillViewModel
-    {
-        public int Id { get; set; }
-        public string SkillName { get; set; }
-        public string Status { get; set; }
-    }
-}
--- Day_10/Mission/Mission.Entities/Models/MissionThemeViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Mission.Entities.Models
-{
-    public class MissionThemeViewModel
-    {
-        public int Id { get; set; }
-        public string ThemeName { get; set; }
-        public string Status { get; set; }
-    }
-}
